316214c7e42f47b74893f11ae30e5d53511220e8
umairdadaa <md1404@live.mdx.ac.uk>
2022-02-17 16:38:18 +0400

    Frontend Submission

---
+++ FINAL Coursework 2 - Frontend/WEBDEV CW 2/assets/js/custom.js
@@ -0,0 +1,24 @@
+
+'use strict';
+$(document).ready(function() {
+
+
+    // Quntity in view product add & subtract
+
+    $('#btn-minus').click(function(){
+      var val = $("#var-value").html();
+      val = (val=='1')?val:val-1;
+      $("#var-value").html(val);
+      $("#product-quanity").val(val);
+      return false;
+    });
+    $('#btn-plus').click(function(){
+      var val = $("#var-value").html();
+      val++;
+      $("#var-value").html(val);
+      $("#product-quanity").val(val);
+      return false;
+    });
+
+
+});
